622a46a37b715412836461982567646b8f3062c0
Son Nguyen <vietson6103@gmail.com>
2018-07-16 10:33:15 +0700

    mảng tăng và giảm dần

---
+++ src/Array.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = {5, 2, 4, 9, 3, 5, 1};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+
+        System.out.println();
+
+        int temp1 = array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp1 = array[j];
+                    array[j] = array[i];
+                    array[i] = temp1;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+
+        System.out.println();
+
+        int temp2 = array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    temp2 = array[j];
+                    array[j] = array[i];
+                    array[i] = temp2;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+    }
+}
