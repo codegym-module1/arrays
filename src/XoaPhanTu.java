12fc5290361128c7ff806b798f781d0718ce1006
Son Nguyen <vietson6103@gmail.com>
2018-07-17 09:33:28 +0700

    thêm xóa phần tử

--- src/XoaPhanTu.java
+++ src/XoaPhanTu.java
@@ -1,23 +1,13 @@
 import java.util.Scanner;
 
-public class ThemPhanTu {
+public class XoaPhanTu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] array = {1, 2, 5, 4, 6, 7, 1};
         printArray(array);
         System.out.println();
-        System.out.print("Nhập số cần chèn: ");
-        int x = scanner.nextInt();
-        System.out.print("Nhập vị trí cần chèn trong mảng: ");
+        System.out.print("Nhập vị trí cần xóa trong mảng: ");
         int index = scanner.nextInt();
-
-        if (index <= 1 && index >= array.length - 1) {
-            for (int i = 0; i < array.length + 1; i++) {
-                System.out.println();
-
-
-            }
-        }
     }
 
     public static void printArray(int[] array) {
